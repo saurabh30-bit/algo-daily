f18ea0636d7a0cbd2103072f5960328a9ed83a7f
saurabh30-bit <sshinde0582@gmail.com>
2026-07-31 11:22:00 +0530

    Add Group Anagrams solution

---
+++ group_anagrams.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    groups = defaultdict(list)
+    for s in strs:
+        key = tuple(sorted(s))
+        groups[key].append(s)
+    return list(groups.values())
